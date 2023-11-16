c16ee2f5a50aab3ca6dbf55e2e26b34b175f770a
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-16 18:00:07 +0500

    DbContext added

---
+++ FirstFluentAPI/DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FirstFluentAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FirstFluentAPI.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Bicycles> Bicycles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetCallingAssembly());
+        }
+    }
+}
