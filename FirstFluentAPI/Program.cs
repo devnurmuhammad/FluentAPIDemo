c5b3c9a7a4cdd0555bb59acacf880f6a8d544c46
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-16 18:04:04 +0500

    fixed

--- FirstFluentAPI/Program.cs
+++ FirstFluentAPI/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
-}); 
+});
 
 var app = builder.Build();
 
